19b799310ee7a5dce7d5413ed3c7a206deb72986
ascourtas <ascourtas@gmail.com>
2017-04-11 11:10:37 -0400

    can now pass user objects across views

--- app/views.py
+++ app/views.py
@@ -7,7 +7,8 @@ def index():
 	users = [ { 'name' : 'Matt Eads', 'DOB' : '04/38/27'}, { 'name' : 'Robert Downey Jr', 'DOB' : '11/27/69'}]
 	return render_template('index.html', users=users)
 
-@app.route('/patientdata')
-def patient_data():
-	return render_template('patient_data.html')
+@app.route('/patientdata/<path:user>')
+def patient_data(user):
+	user = eval(user)
+	return render_template('patient_data.html', user=user)
 
