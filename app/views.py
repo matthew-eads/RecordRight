676a396a76fba66c7f01959c07f6bc4446c75be4
Azmina Karukappadath <azmina.karukappadath@tufts.edu>
2017-04-11 01:06:51 -0400

    updating with patient data view

--- app/views.py
+++ app/views.py
@@ -1,6 +1,13 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+	users = [ { 'name' : 'Matt Eads', 'DOB' : '04/38/27'}, { 'name' : 'Robert Downey Jr', 'DOB' : '11/27/69'}]
+	return render_template('index.html', users=users)
+
+@app.route('/patientdata')
+def patient_data():
+	return render_template('patient_data.html')
+
