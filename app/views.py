f28bfad922f74d28511edc3132f329138932999b
ascourtas <ascourtas@gmail.com>
2017-04-18 02:08:15 -0400

    include template inheritance, set up login redirect

--- app/views.py
+++ app/views.py
@@ -1,6 +1,13 @@
+from __future__ import print_function
 from app import app
-from flask import render_template
+from flask import render_template, redirect
 from .forms import LoginForm
+from wtforms import Form, validators
+import logging
+
+
+import sys
+
 
 @app.route('/')
 @app.route('/index')
@@ -10,10 +17,12 @@ def index():
 		{ 'name' : 'Robert Downey Jr', 'DOB' : '11/27/69', 'hx' : 'History of hyperextensible knees.'},
 		{ 'name' : 'Sir Biddly-Bop III', 'DOB' : '3/3/33', 'hx' : 'History of fibromyalgia, smoking, GERD.'},
 		]
+	# print("Can I see this?", file=sys.stderr)
 	return render_template('index.html', users=users)
 
 @app.route('/patientdata/<path:user>')
 def patient_data(user):
+	sys.stderr.write("Can is SEE?")
 	# this converts the user variable, which is a string, into a dict
 	user = eval(user)
 	return render_template('patient_data.html', user=user)
@@ -21,5 +30,11 @@ def patient_data(user):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	form = LoginForm()
-	return render_template('login.html', title='SignIn', form=form)
+	sys.stderr.write("FORM IS %s \n" % form.is_remembered)
+	sys.stderr.write("\n")
+	if form.validate() and request.method == 'POST':
+		flash('Login requested for User=%s, is_remembered=%s' % form.username.data, str(form.is_remembered.data))
+		sys.stderr.write("AFTER FLASH \n")
+		return redirect('/index')
+	return render_template('login.html', title="SignIn", form=form)
 
