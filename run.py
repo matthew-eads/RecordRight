7465aaa242a0d9476793f7ac668a45bfc8aaa9a8
ascourtas <ascourtas@gmail.com>
2017-04-17 22:42:53 -0400

    edited run.py to run program at localhost:5000

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
 #!flask/bin/python
 from app import app
-app.run(debug=True, host='192.168.124.137')
+# is there a particular reason this was altered?
+# app.run(debug=True, host='192.168.124.137')
+app.run(debug=True)
