bb3e6c83bc14bb4a43a9bfa51ab8c07b1908bcd3
ascourtas <ascourtas@gmail.com>
2017-04-18 11:36:31 -0400

    db now works, but migrations DO NOT yet work

--- app/database.py
+++ app/database.py
@@ -3,6 +3,8 @@ from config import SQLALCHEMY_DATABASE_URI
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+import sys
+
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 # this is actually our session -- I need to rename this
 db = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
@@ -12,5 +14,6 @@ Base.query = db.query_property()
 
 def init_db():
 	import app.models
+	sys.stderr.write('INTIALIZING DB \n \n \n')
 	# actually creates the db -- should only run this once
 	Base.metadata.create_all(bind=engine)
--- app/models.py
+++ app/models.py
@@ -6,6 +6,8 @@ class User(Base):
 	id = Column(Integer, primary_key=True)
 	username = Column(String(8), index=True)
 	password = Column(String(8))
+	def __repr__(self):
+		return '<User %r>'%(self.username)
 
 class Patient(Base):
 	__tablename__ = 'patients'
@@ -14,3 +16,5 @@ class Patient(Base):
 	DOB = Column(String, index=True)
 	hx = Column(String, index=True)
 
+	def __repr__(self):
+		return '<Patient %r %r %r>'%(self.name, self.DOB, self.hx)
