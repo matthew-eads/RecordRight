8acc9dc23074d58bd5bebbdd739aa03d7154c9d6
Azmina Karukappadath <azmina.karukappadath@tufts.edu>
2017-04-18 17:28:18 -0400

    working on search functionality

--- app/models.py
+++ app/models.py
@@ -1,5 +1,8 @@
 from sqlalchemy import *
 from app.database import Base
+from config import WHOOSH_BASE
+from whooshalchemy import IndexService
+from database import db
 
 class User(Base):
 	__tablename__ = 'users'
@@ -11,6 +14,7 @@ class User(Base):
 
 class Patient(Base):
 	__tablename__ = 'patients'
+	__searchable__ = ['name']
 	id = Column(Integer, primary_key=True)
 	name = Column(String, index=True)
 	DOB = Column(String, index=True)
@@ -18,3 +22,32 @@ class Patient(Base):
 
 	def __repr__(self):
 		return '<Patient %r %r %r>'%(self.name, self.DOB, self.hx)
+
+from app import app
+
+import sys
+if sys.version_info >= (3, 0):
+    enable_search = False
+else:
+    enable_search = True
+    import whooshalchemy
+
+config = {"WHOOSH_BASE": WHOOSH_BASE}
+
+index_service = IndexService(config=config, session = db)
+index_service.register_class(Patient)
+
+
+# class Post(db.Model):
+#     __searchable__ = ['body']
+
+#     id = Column(Integer, primary_key=True)
+#     body = Column(String(140))
+#     timestamp = Column(DateTime)
+#     user_id = Column(Integer, ForeignKey('user.id'))
+
+#     def __repr__(self):
+#         return '<Post %r>' % (self.body)
+
+# if enable_search:
+#     whooshalchemy.whoosh_index(app, Patient)
