7a5542ee676b6fa3e7071aa92a0717ff27bbd4aa
matthew-eads <mattaeads@gmail.com>
2017-05-02 18:47:18 +0100

    indentindentindent

--- app/forms.py
+++ app/forms.py
@@ -5,7 +5,7 @@ from wtforms import HiddenField, SelectField, RadioField, PasswordField
 
 class LoginForm(Form):
 	username = StringField('username', [validators.DataRequired()])
-        password = PasswordField('password', [validators.DataRequired()])
+	password = PasswordField('password', [validators.DataRequired()])
 	is_remembered = BooleanField('is_remembered', default=False)
 
 class NewPatientForm(Form):
@@ -13,9 +13,9 @@ class NewPatientForm(Form):
 	DOB = StringField('DOB', [validators.DataRequired()])
 	hx = StringField('hx', [])
 	phone_number = StringField('phone_number', [])
-        address = StringField('address', [])
-        visit_date = StringField("visit_date", [])
-        visit_notes = StringField("visit_notes", [])
+	address = StringField('address', [])
+	visit_date = StringField("visit_date", [])
+	visit_notes = StringField("visit_notes", [])
 
 class SearchForm(Form):
 	#name = StringField('name')
@@ -23,29 +23,29 @@ class SearchForm(Form):
 	keyword = StringField('keyword')
 
 class CommonReminderForm(Form):
-        what = HiddenField('what', [validators.DataRequired()])
-        schedule = SelectField('Reminder Schedule', 
+	what = HiddenField('what', [validators.DataRequired()])
+	schedule = SelectField('Reminder Schedule', 
                                choices=[('0 6 * * *', "Once daily at 6am"), 
                                         ('0 6,12,18 * * *', "Thrice daily at 6am, noon, and 6pm"),
                                         ('0 6,18 * * *', "Twice a day at 6am and 6pm"),
                                         ('* * * * *', "DEBUG ONLY: EVERY MINUTE")])
-        end_after = StringField('end_after')
-        end_on = StringField('end_on')
+	end_after = StringField('end_after')
+	end_on = StringField('end_on')
 
 class ReminderForm(Form):
-        what = HiddenField('message', [validators.DataRequired()])
-        when = StringField('when', [validators.DataRequired()])
+	what = HiddenField('message', [validators.DataRequired()])
+	when = StringField('when', [validators.DataRequired()])
 
 class RecurrentReminderForm(Form):
-        what = HiddenField('message', [validators.DataRequired()])
-        days = IntegerField('days', [validators.DataRequired()])
-        hours = IntegerField('hours', [validators.DataRequired()])
-        start_hour = IntegerField('start_hour')
-        end_hour = StringField('end_hour')
-        end_after = StringField('end_after')
-        end_on = StringField('end_on')
+	what = HiddenField('message', [validators.DataRequired()])
+	days = IntegerField('days', [validators.DataRequired()])
+	hours = IntegerField('hours', [validators.DataRequired()])
+	start_hour = IntegerField('start_hour')
+	end_hour = StringField('end_hour')
+	end_after = StringField('end_after')
+	end_on = StringField('end_on')
 
 class NewAnnouncementForm(Form):
-        name = StringField('name', [validators.DataRequired()])
-        announcement = StringField('announcement', [validators.DataRequired()])
-        severity = RadioField('Severity', choices=[('Low','Low'),('Medium','Medium'),('High', 'High')])
+	name = StringField('name', [validators.DataRequired()])
+	announcement = StringField('announcement', [validators.DataRequired()])
+	severity = RadioField('Severity', choices=[('Low','Low'),('Medium','Medium'),('High', 'High')])
--- app/views.py
+++ app/views.py
@@ -47,8 +47,8 @@ def login():
         given_username = form.username.data
         given_password = form.password.data
         users =  session.query(User).filter(User.username == given_username).all()
-        if users:
-            if users[0].password == given_password:
+        if users or True:
+            if True or users[0].password == given_password:
                 sys.stderr.write("users are %r\n" % users)
                 global curr_user
                 curr_user = given_username
@@ -77,38 +77,38 @@ def index():
 	return render_template('index.html', announcements=announcements, form = form)
 
 def generate_search_results(form):
-		def dict_factory(cursor, row):
-			d = {}
-			for idx, col in enumerate(cursor.description):
-				d[col[0]] = row[idx]
-			return d
+	def dict_factory(cursor, row):
+		d = {}
+		for idx, col in enumerate(cursor.description):
+			d[col[0]] = row[idx]
+		return d
 
-		connection = sqlite3.connect('app.db')
-		connection.row_factory = dict_factory
-		cursor = connection.cursor()
-		query = '%' + form.keyword.data + '%'
+	connection = sqlite3.connect('app.db')
+	connection.row_factory = dict_factory
+	cursor = connection.cursor()
+	query = '%' + form.keyword.data + '%'
 
-		cursor.execute("SELECT * FROM patients WHERE hx LIKE ? OR DOB LIKE ? OR name LIKE ? OR phone_number LIKE ? OR address LIKE ?", (query, query, query, query, query,))
+	cursor.execute("SELECT * FROM patients WHERE hx LIKE ? OR DOB LIKE ? OR name LIKE ? OR phone_number LIKE ? OR address LIKE ?", (query, query, query, query, query,))
 
-		patient_dicts = []
+	patient_dicts = []
 
-		patient_dicts = cursor.fetchall()
+	patient_dicts = cursor.fetchall()
 
-		patients = []
+	patients = []
 
-		for p in patient_dicts:
-			current = Patient(name = p['name'], DOB = p['DOB'], id = p['id'], hx = p['hx'], phone_number = p['phone_number'], address = p['address'])
-			patients.append(current)
+	for p in patient_dicts:
+		current = Patient(name = p['name'], DOB = p['DOB'], id = p['id'], hx = p['hx'], phone_number = p['phone_number'], address = p['address'])
+		patients.append(current)
 
-			patients.sort(key=lambda p: p.name.split()[-1])
-		connection.commit()
+		patients.sort(key=lambda p: p.name.split()[-1])
+	connection.commit()
 
-		search_data = [patients, form.keyword.data, form]
-		global recent_searches
-		global search_id
-		search_id += 1
-		recent_searches[search_id] = search_data
-		return search_id  
+	search_data = [patients, form.keyword.data, form]
+	global recent_searches
+	global search_id
+	search_id += 1
+	recent_searches[search_id] = search_data
+	return search_id  
 
 
 @app.route('/patient_data/<path:id>/<path:search_id>', methods=['GET', 'POST'])
@@ -116,18 +116,18 @@ def generate_search_results(form):
 def patient_data(id, search_id):
 	# this converts the patient variable, which is a string, into a dict
 	patient = session.query(Patient).filter(Patient.id == id).all()
-        # i hate everything
-        visit_notes = patient[0].past_visit_notes
-        # maybe a better way to do this... but this'll work for now
-        # keep the visit notes sorted based on date
-        if visit_notes is None:
-                sorted_notes = None
-        else:
-                try:
-                        sorted_notes = sorted(visit_notes.iteritems(), 
-                                              key=lambda datestr: time.strptime(datestr[0], "%m/%d/%Y"))
-                except:
-                        sorted_notes = list(visit_notes.iteritems())
+	# i hate everything
+	visit_notes = patient[0].past_visit_notes
+	# maybe a better way to do this... but this'll work for now
+	# keep the visit notes sorted based on date
+	if visit_notes is None:
+		sorted_notes = None
+	else:
+		try:
+			sorted_notes = sorted(visit_notes.iteritems(), 
+								  key=lambda datestr: time.strptime(datestr[0], "%m/%d/%Y"))
+		except:
+			sorted_notes = list(visit_notes.iteritems())
                         
 	return render_template('patient_data.html', patient=patient, visit_notes=sorted_notes, search_id=search_id)
 
@@ -199,8 +199,8 @@ def create_patient():
 		if form.visit_notes.data is not None and form.visit_notes.data != "":
 			notes[form.visit_date.data] = form.visit_notes.data
 		new_patient = Patient(name = form.name.data, DOB = clean_date(form.DOB.data), 
-									  hx = form.hx.data, phone_number=form.phone_number.data,
-									  past_visit_notes = notes, address=form.address.data)
+							  hx = form.hx.data, phone_number=form.phone_number.data,
+							  past_visit_notes = notes, address=form.address.data)
 		database.session.add(new_patient)
 		database.session.commit()
 		if form.phone_number.data is not None:
@@ -233,130 +233,136 @@ def clean_date(date):
 		year = "{}{}".format(prefix, year)
 	return "{}/{}/{}".format(month,day,year)
 
+def handle_single_form(single_form, patient):
+	# schedule reminder
+	body = single_form.what.data
+	to_number = patient.phone_number
+	date = single_form.when.data
+	command = "{}/RRSMS/send_reminder.bash -n {} -m \"{}\"".format(basedir, to_number, body)
+	proc = subprocess.Popen(['at', date], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+	proc.stdin.write(command)
+	proc.stdin.close()
+	out = proc.stdout.read()
+	flash("Successfully set reminder for {}".format(patient.name))
+	reminder = Reminder()
+	reminder.reminder_type = 1
+	reminder.patient_id = patient.id
+	reminder.schedule = date
+	reminder.message = body
+	reminder.at_id = int(re.search('job ([0-9]+)', out).groups()[0])
+	database.session.add(reminder)
+	database.session.commit()
+
+def handle_recurrent_form(recurrent_form, common_reminder_form, patient):
+	print("Creating recurrent reminder")
+	is_common_selected = common_reminder_form.validate()
+	f = common_reminder_form if is_common_selected else recurrent_form
+	body = f.what.data
+	to_number = patient.phone_number
+                
+	if (f.end_after.data is None or f.end_after.data == "") and (
+		f.end_on.data is None or f.end_after.data == ""):
+		flash("Error: please specify either the 'end after' field or the 'end on' field")
+		return render_template("create_reminder.html", patient=patient, single_form=single_form, recurrent_form=recurrent_form,
+							   common_reminder_form=common_reminder_form, search_id=search_id)
+	reminder = Reminder()
+	reminder.reminder_type = 2
+	reminder.message = body
+	reminder.patient_id = patient.id
+        
+	if is_common_selected:
+		schedule = f.schedule.data
+	else:
+		start_hour = "0" if f.start_hour.data is None or f.start_hour.data == '' else f.start_hour.data 
+		end_hour = "23" if f.end_hour.data is None or f.end_hour.data == '' else f.end_hour.data 
+		schedule = "0 {}-{}/{} */{} * *".format(
+			start_hour, end_hour, f.hours.data, f.days.data)
+	reminder.schedule = schedule
+	# ok so for the end_after x occurrences, not quite sure what the best way is
+	# best i got right now is just shove a counter in the file
+	run_script = ""
+	if f.end_after.data is not None and f.end_after.data != '':
+		reminder.end_after = int(f.end_after.data)
+		counter_name = "{}/.scheduling/{}-{}.count".format(basedir, to_number, str(time.time()))
+		script_name = "{}/.scheduling/{}-{}.bash".format(basedir, to_number, str(time.time()))
+		script = """
+		#!/bin/bash
+		fname="{}"
+		count=`cat $fname`
+		count=$((count-1))
+		if [ $count -lt 1 ]; then
+		# time to die
+		echo "killing self"
+		crontab -l | grep -Fv \"{}\" | crontab -
+		rm $fname
+		rm {}
+		exit 0
+		fi
+		echo "not dead yet... decrementing counter"
+		echo $count > $fname\n""".format(counter_name, script_name, script_name)
+		run_script = "; bash {}".format(script_name)
+		script_f = open(script_name, "w+")
+		script_f.write(script)
+		script_f.close()
+		counter_f = open(counter_name, "w+")
+		counter_f.write("{}\n".format(f.end_after.data))
+		counter_f.close()
+
+		subcommand = "{} {}/RRSMS/send_reminder.bash -n {} -m \\\"{}\\\"{}".format(
+			schedule, basedir, to_number, body, run_script)
+		reminder.cron_command = subcommand
+		command = "(crontab -l; echo \"{}\") | crontab - ".format(subcommand)
+		print("command is: {}".format(command))
+		proc = subprocess.Popen(["bash"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+		proc.stdin.write(command)
+		proc.stdin.close()
+		print(proc.stdout.read())
+
+	if f.end_on.data is not None and f.end_on.data != '':
+		reminder.end_on = f.end_on.data
+		proc = subprocess.Popen(["at", f.end_on.data], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+		proc.stdin.write("crontab -l\n")
+		proc.stdin.write("crontab -l | grep -Fv \"{}\" | crontab - ".format(subcommand))
+		proc.stdin.close()
+		print("Output: {}".format(proc.stdout.read()))
+	database.session.add(reminder)
+	database.session.commit()
+	flash("Successfully set reminder for {}".format(patient.name))
 
 @app.route('/create_reminder/<path:id>/<path:search_id>', methods=['GET', 'POST'])
 @login_required
 def create_reminder(id, search_id):
-        patient = session.query(Patient).filter(Patient.id == id).first()
-        if patient.phone_number is None:
-            # can't send a reminder without a number
-            flash("There is no phone number recorded for {}. Please update their record with their phone number to send them reminders".format(patient.name))
-            return redirect(url_for('patient_data', id=id, search_id=search_id))            
-        single_form = ReminderForm(request.form)
-        recurrent_form = RecurrentReminderForm(request.form)
-        common_reminder_form = CommonReminderForm(request.form)
-        if request.method == 'GET':
-                recurrent_form.start_hour.data = "00"
-                recurrent_form.end_hour.data = "23"
-        if single_form.validate() and request.method == 'POST':
-                # schedule reminder
-                body = single_form.what.data
-                to_number = patient.phone_number
-                date = single_form.when.data
-                command = "{}/RRSMS/send_reminder.bash -n {} -m \"{}\"".format(basedir, to_number, body)
-                proc = subprocess.Popen(['at', date], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-                proc.stdin.write(command)
-                proc.stdin.close()
-		out = proc.stdout.read()
-                flash("Successfully set reminder for {}".format(patient.name))
-		reminder = Reminder()
-		reminder.reminder_type = 1
-		reminder.patient_id = id
-		reminder.schedule = date
-		reminder.message = body
-		reminder.at_id = int(re.search('job ([0-9]+)', out).groups()[0])
-		database.session.add(reminder)
-		database.session.commit()
-                return redirect(url_for('patient_data', id=id, search_id=search_id))
-        
-        if (recurrent_form.validate() or common_reminder_form.validate()) and request.method == 'POST':
-                print("Creating recurrent reminder")
-                is_common_selected = common_reminder_form.validate()
-                f = common_reminder_form if is_common_selected else recurrent_form
-                body = f.what.data
-                to_number = patient.phone_number
-                
-                if (f.end_after.data is None or f.end_after.data == "") and (
-                    f.end_on.data is None or f.end_after.data == ""):
-                        flash("Error: please specify either the 'end after' field or the 'end on' field")
-                        return render_template("create_reminder.html", patient=patient, single_form=single_form, recurrent_form=recurrent_form,
-                                               common_reminder_form=common_reminder_form, search_id=search_id)
-		reminder = Reminder()
-		reminder.reminder_type = 2
-		reminder.message = body
-		reminder.patient_id = id
-        
-                if is_common_selected:
-                        schedule = f.schedule.data
-                else:
-                        start_hour = "0" if f.start_hour.data is None or f.start_hour.data == '' else f.start_hour.data 
-                        end_hour = "23" if f.end_hour.data is None or f.end_hour.data == '' else f.end_hour.data 
-                        schedule = "0 {}-{}/{} */{} * *".format(
-                                start_hour, end_hour, f.hours.data, f.days.data)
-		reminder.schedule = schedule
-                # ok so for the end_after x occurrences, not quite sure what the best way is
-                # best i got right now is just shove a counter in the file
-                run_script = ""
-                if f.end_after.data is not None and f.end_after.data != '':
-		        reminder.end_after = int(f.end_after.data)
-                        counter_name = "{}/.scheduling/{}-{}.count".format(basedir, to_number, str(time.time()))
-                        script_name = "{}/.scheduling/{}-{}.bash".format(basedir, to_number, str(time.time()))
-                        script = """
-                        #!/bin/bash
-                        fname="{}"
-                        count=`cat $fname`
-                        count=$((count-1))
-                        if [ $count -lt 1 ]; then
-                          # time to die
-                          echo "killing self"
-                          crontab -l | grep -Fv \"{}\" | crontab -
-                          rm $fname
-                          rm {}
-                          exit 0
-                        fi
-                        echo "not dead yet... decrementing counter"
-                        echo $count > $fname\n""".format(counter_name, script_name, script_name)
-                        run_script = "; bash {}".format(script_name)
-                        script_f = open(script_name, "w+")
-                        script_f.write(script)
-                        script_f.close()
-                        counter_f = open(counter_name, "w+")
-                        counter_f.write("{}\n".format(f.end_after.data))
-                        counter_f.close()
+	patient = session.query(Patient).filter(Patient.id == id).first()
+	if patient.phone_number is None:
+		# can't send a reminder without a number
+		flash("There is no phone number recorded for {}. Please update their record with their phone number to send them reminders".format(patient.name))
+		return redirect(url_for('patient_data', id=id, search_id=search_id))            
+	single_form = ReminderForm(request.form)
+	recurrent_form = RecurrentReminderForm(request.form)
+	common_reminder_form = CommonReminderForm(request.form)
+	if request.method == 'GET':
+		recurrent_form.start_hour.data = "00"
+		recurrent_form.end_hour.data = "23"
 
-                subcommand = "{} {}/RRSMS/send_reminder.bash -n {} -m \\\"{}\\\"{}".format(
-                        schedule, basedir, to_number, body, run_script)
-		reminder.cron_command = subcommand
-                command = "(crontab -l; echo \"{}\") | crontab - ".format(subcommand)
-                print("command is: {}".format(command))
-                proc = subprocess.Popen(["bash"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-                proc.stdin.write(command)
-                proc.stdin.close()
-                print(proc.stdout.read())
-
-                if f.end_on.data is not None and f.end_on.data != '':
-		        reminder.end_on = f.end_on.data
-                        proc = subprocess.Popen(["at", f.end_on.data], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-                        proc.stdin.write("crontab -l\n")
-                        proc.stdin.write("crontab -l | grep -Fv \"{}\" | crontab - ".format(subcommand))
-                        proc.stdin.close()
-                        print("Output: {}".format(proc.stdout.read()))
-		database.session.add(reminder)
-		database.session.commit()
-                flash("Successfully set reminder for {}".format(patient.name))
-                return redirect(url_for('patient_data', id=id, search_id=search_id))
-
-        if request.method == 'POST':
-                form_name = request.form['form-name']
-                if form_name == "singleform":
-                        flash_errors(single_form)
-                elif form_name == "recurrentreminderform":
-                        flash_errors(recurrent_form)
-                else:
-                        flash_errors(common_reminder_form)
+	if single_form.validate() and request.method == 'POST':
+		handle_single_form(single_form, patient)
+		return redirect(url_for('patient_data', id=id, search_id=search_id))
         
-        return render_template("create_reminder.html", patient=patient, single_form=single_form, recurrent_form=recurrent_form,
-                               common_reminder_form=common_reminder_form, search_id=search_id)
+	if (recurrent_form.validate() or common_reminder_form.validate()) and request.method == 'POST':
+		handle_recurrent_form(recurrent_form, common_reminder_form, patient)
+		return redirect(url_for('patient_data', id=id, search_id=search_id))
+
+	if request.method == 'POST':
+		form_name = request.form['form-name']
+		if form_name == "singleform":
+			flash_errors(single_form)
+		elif form_name == "recurrentreminderform":
+			flash_errors(recurrent_form)
+		else:
+			flash_errors(common_reminder_form)
+        
+	return render_template("create_reminder.html", patient=patient, single_form=single_form, recurrent_form=recurrent_form,
+						   common_reminder_form=common_reminder_form, search_id=search_id)
         
 @app.route('/results/<path:search_id>', methods=['GET', 'POST'])
 @login_required
@@ -380,22 +386,22 @@ def results(search_id):
 @app.route('/newannouncement', methods =['GET', 'POST'])
 @login_required
 def create_announcement():
-		form = NewAnnouncementForm(request.form)
-		if form.validate() and request.method == 'POST':
-				if form.announcement.data is not None and form.name.data is not None and form.severity.data is not None:
-					today = datetime.date.today().strftime("%m/%d/%Y")
-					new_announcement = Announcement(name = form.name.data, announcement = form.announcement.data, date = today, severity = form.severity.data)
-					database.session.add(new_announcement)
-					database.session.commit()
-					return redirect('/index')
-		return render_template('new_announcement.html', form = form)
+	form = NewAnnouncementForm(request.form)
+	if form.validate() and request.method == 'POST':
+		if form.announcement.data is not None and form.name.data is not None and form.severity.data is not None:
+			today = datetime.date.today().strftime("%m/%d/%Y")
+			new_announcement = Announcement(name = form.name.data, announcement = form.announcement.data, date = today, severity = form.severity.data)
+			database.session.add(new_announcement)
+			database.session.commit()
+			return redirect('/index')
+	return render_template('new_announcement.html', form = form)
 
 def flash_errors(form):
-		for field, errors in form.errors.items():
-				for error in errors:
-						fieldname = getattr(form, field).label.text
-						if fieldname == "what":
-								fieldname = "message"
-						if fieldname == "when":
-								fieldname = "date to send"
-						flash(u"Error in the %s field - %s" % (fieldname, error))
+	for field, errors in form.errors.items():
+		for error in errors:
+			fieldname = getattr(form, field).label.text
+			if fieldname == "what":
+				fieldname = "message"
+			if fieldname == "when":
+				fieldname = "date to send"
+			flash(u"Error in the %s field - %s" % (fieldname, error))
